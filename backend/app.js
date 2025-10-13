3a78f8fda16ca3bbac350e2c40142dfd3d948cb3
aryan-sharma-1908 <aryansharma81828@gmail.com>
2025-10-14 03:36:25 +0530

    .

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const registerController = require('./controllers/register.controller.js');
 const profileRoute = require('./routes/profile.route.js');
 const profileController = require('./controllers/profile.controller.js');
 const checkAuth = require('./middlewares/auth.middleware.js');
-const whiteList = ['http://localhost:5173', 'http://127.0.0.1:5173','https://www.mongodb.com/docs/atlas/security-whitelist/', 'https://mental-awareness-2yh8mzchz-aryan-sharmas-projects-62cf0133.vercel.app/', 'https://mental-awareness-app.onrender.com'];
+const whiteList = ['http://localhost:5173', 'http://127.0.0.1:5173','https://www.mongodb.com/docs/atlas/security-whitelist', 'https://mental-awareness-2yh8mzchz-aryan-sharmas-projects-62cf0133.vercel.app', 'https://mental-awareness-app-git-main-aryan-sharmas-projects-62cf0133.vercel.app'];
 const corsOptions = {
     origin: function(origin, callback) {
          if(!origin) return callback(null, true);
