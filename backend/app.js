9ea57290b19308d56905226585f4cec240057900
aryan-sharma-1908 <aryansharma81828@gmail.com>
2025-10-14 02:04:03 +0530

    build error fixing

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const registerController = require('./controllers/register.controller.js');
 const profileRoute = require('./routes/profile.route.js');
 const profileController = require('./controllers/profile.controller.js');
 const checkAuth = require('./middlewares/auth.middleware.js');
-const whiteList = ['http://localhost:5173', 'http://127.0.0.1:5173'];
+const whiteList = ['http://localhost:5173', 'http://127.0.0.1:5173',' https://www.mongodb.com/docs/atlas/security-whitelist/'];
 const corsOptions = {
     origin: function(origin, callback) {
          if(!origin) return callback(null, true);
@@ -29,6 +29,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
 
+app.use('/',(req,res) => {
+    res.send('Server is running';)
+})
 app.use('/register', registerController);
 app.use('/login', loginController);
 app.use('/profile', checkAuth, profileController);
